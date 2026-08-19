81b3b6adec29d8b45dbf28145ecc5ced8a00f1f3
DEV • ZIKKY 🤍 <zikkystar0001@gmail.com>
2026-08-19 23:56:15 +0100

    Change import to "teleproto/sessions/index.js";

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import path from "node:path";
 import crypto from "node:crypto";
 
 import { TelegramClient } from "teleproto";
-import { StringSession } from "teleproto/sessions";
+import { StringSession } from "teleproto/sessions/index.js";
 
 
 // ============================================================
